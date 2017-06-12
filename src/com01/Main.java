53eabe6924986f7f8c5e904a28d26072ae0cb071
PeterPanTrue <397226804@qq.com>
2017-06-12 21:30:11 +0800

    no2

--- src/com01/Main.java
+++ src/com01/Main.java
@@ -2,7 +2,7 @@ package com01;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hahahaha");
+		System.out.println("haha");
 		System.out.println("33333333");
 	}
 	public void say(){
