e0c9f02511a6e4765362a1d3cedb27e73139a768
PeterPanTrue <397226804@qq.com>
2017-11-05 15:38:45 +0800

    idea提交2

--- src/com01/Main.java
+++ src/com01/Main.java
@@ -20,6 +20,7 @@ public class Main {
 		System.out.println(day.getDays());
 		System.out.println(day.getDays());
 		System.out.println(day.getDays());
+		System.out.println(day.getDays());
 
 	} 
 	
